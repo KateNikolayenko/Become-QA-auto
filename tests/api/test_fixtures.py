2d3d846badfefb2f52f719b48995a7f6bb2b9767
Kate Nikolayenko <nikolayenko_kate@ukr.net>
2023-01-30 10:49:02 -0400

    Http tests and fixture tests added

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,29 +1,10 @@
-class User:
+import pytest
 
-    def __init__(self) -> None:
-        self.name = None
-        self.second_name = None
-    
-
-    def create(self):
-        self.name = "Kate"
-        self.lastname = "Niko"
-
-    def remove(self):
-        self.name = ''
-        self.lastname = ''
-
-def test_change_name():
-    user = User()
-    user.create()
-
+@pytest.mark.check
+def test_change_name(user):
     assert user.name == "Kate"
-    user.remove()
 
-def test_change_lastname():
-    user = User()
-    user.create()
-
-    assert user.second_name == "Niko"
-    user.remove()
     
+@pytest.mark.check
+def test_change_lastname(user):
+    assert user.lastname == "Niko"
--- tests/ui/test_api.py
+++ tests/ui/test_api.py
@@ -1,26 +1,14 @@
 import pytest
 
-
-class User:
-
-    def __init__(self) -> None:
-        self.name = "Kate"
-        self.lastname = "Niko"
-
-
-@pytest.fixture
-def user():
-    yield User()
-    
-
+@pytest.mark.change
 def test_remove_name(user):
     user.name = ''
     assert user.name == ''
 
- 
+@pytest.mark.check
 def test_name(user):
     assert user.name == "Kate"
 
-
+@pytest.mark.check
 def test_lastname(user):
     assert user.lastname == "Niko"
